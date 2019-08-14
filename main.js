59d32835ccecc6c3b2a33074ba00e921b829c06d
antodg95 <antodg95@gmail.com>
2019-08-14 14:55:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -24,6 +24,11 @@ function onLoad() {
 
 }
 
+/**
+ * Analize the given input text, if it's a 5 digit string run a ZIP research,
+ * otherwise run research by city name.
+ * @param {string} inputText input text given by input form.
+ */
 function inputCheck(inputText) {
   if (inputText == parseInt(inputText) && inputText.length == 5) {
     getApiText('zip', inputText);
@@ -32,6 +37,17 @@ function inputCheck(inputText) {
   }
 
 }
+
+
+/**
+ * Open an XMLHttpRequest with OpenWeatherMap API using city name or city ZIP.
+ * XMLHttpRequest return a json file.
+ * Without error runs analizeData(weather)
+ * With error runs attachError("City or ZIP Code not found, try again")
+ *
+ * @param {q or ZIP} searchType type of search, by city ('q') or by ZIP Code ('zip')
+ * @param {string} inputText Input text string, city name or zip code.
+ */
 
 function getApiText(searchType, inputText) {
   var requestURL = `https://api.openweathermap.org/data/2.5/forecast?${searchType}=${inputText}&APPID=08874f09563464fbad6669d17899a3e4&units=${unit}`;
@@ -51,11 +67,18 @@ function getApiText(searchType, inputText) {
 
 }
 
+/**
+ * Function to analize datas returned by XMLHttpRequest.
+ * @param {XMLHttpRequest.response.json} weather Weather information
+ */
+
 function analizeData(weather) {
   var resultDiv = document.getElementById('result');
   var input = document.getElementById('search-input');
   input.value = "";
   var cards = document.getElementsByClassName('card');
+
+  //First clean up previous result
   for (var card of cards) {
     while (card.firstChild) {
       card.removeChild(card.firstChild);
@@ -65,10 +88,12 @@ function analizeData(weather) {
     resultDiv.removeChild(resultDiv.firstChild);
   }
 
+  //Append city name and country above carousel
   var h1 = document.createElement('h1');
   h1.appendChild(document.createTextNode(weather.city.name + ', ' + weather.city.country));
   resultDiv.appendChild(h1);
 
+  //Name all carousel's cards with namedays.
   var days = document.getElementsByClassName('day');
   days[0].innerHTML = "Today";
   for (var i = 1; i < days.length; i++) {
@@ -78,9 +103,10 @@ function analizeData(weather) {
     days[i].innerHTML = numberToNameDay(parsedDay);
   }
 
-  var dayS = "nmde";
 
-  var dt_hour = weather.list[0].dt_txt.slice(11, 13);
+  //Fill the first card with the day remaining information.
+  var dayS = "nmde";
+  var dt_hour = weather.list[0].dt_txt.slice(11, 13); //Take the first hour information
   var letter = daySection(dt_hour);
   var card1 = cards[0];
   var daySec = daySectionToName(letter);
@@ -132,6 +158,7 @@ function analizeData(weather) {
     m = m+2;
   }
 
+  //Find the beginning of the next day.
   for (var i = 1; i<weather.list.length; i++) {
     if(weather.list[i].dt_txt.slice(11, 13)==0) {
       m=i;
@@ -139,6 +166,7 @@ function analizeData(weather) {
     }
   }
 
+  //Fill the remaining 4 cards with full day informations.
   for (var i = 1; i < 5; i++) {
     for (var char of dayS) {
       var weatherSection = weather.list[m];
@@ -190,6 +218,13 @@ function analizeData(weather) {
 
 }
 
+
+/**
+ * Print under the search input text the error.
+ * Before that clean up the previous search of all cards in the carousel.
+ *
+ * @param {string} textError String containing the error message.
+ */
 function attachError(textError) {
   var resultDiv = document.getElementById('result');
   while (resultDiv.firstChild) {
@@ -210,6 +245,10 @@ function attachError(textError) {
   resultDiv.appendChild(h1);
 }
 
+
+/**
+ * Get user current position (lat and lon) and pass it to showPosition().
+ */
 function getApiLocation() {
   var x = document.getElementById('result');
   if (navigator.geolocation) {
@@ -219,7 +258,16 @@ function getApiLocation() {
   }
 }
 
+/**
+ * Open an XMLHttpRequest with OpenWeatherMap API using lat e lon taken by getApiLocation().
+ * XMLHttpRequest return a json file.
+ * Without error runs analizeData(weather)
+ * With error runs attachError("City or ZIP Code not found, try again")
+ *
+ * @param position User current position.
+ */
 function showPosition(position) {
+  console.log("ciaociao");
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
   var requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=08874f09563464fbad6669d17899a3e4&units=${unit}`;
@@ -238,28 +286,19 @@ function showPosition(position) {
   request.send();
 }
 
-function daySectionToName(letter) {
-  switch (letter) {
-    case 'n':
-      return 'night';
-      break;
-    case 'm':
-      return 'morning';
-      break;
-    case 'd':
-      return 'day';
-      break;
-    case 'e':
-      return 'evening';
-      break;
-    default:
-      return 'night';
-  }
-}
 
-function daySection(letter) {
+/**
+ * Divide a day in 4 section:
+ * 00:00 - 05:59 : Night time ('n')
+ * 06:00 - 11:59 : Morning time ('m')
+ * 12:00 - 17:59 : Day time ('d')
+ * 18:00 - 23:59 : Evening time ('e')
+ *
+ * @return Given an hour return the day section letter.
+ */
+function daySection(hour) {
 
-  var floor = Math.floor(letter/ 6.0) * 6;
+  var floor = Math.floor(hour/ 6.0) * 6;
   switch (floor) {
     case 0:
       return 'n';
@@ -279,6 +318,35 @@ function daySection(letter) {
   }
 }
 
+/**
+ *  Transform the daySection letter into the day section name, used to create
+ *  class in HTML.
+ *  @param {daySection} letter letter return by daySection
+ *  @return {string} day section name.
+ */
+function daySectionToName(letter) {
+  switch (letter) {
+    case 'n':
+      return 'night';
+      break;
+    case 'm':
+      return 'morning';
+      break;
+    case 'd':
+      return 'day';
+      break;
+    case 'e':
+      return 'evening';
+      break;
+    default:
+      return 'night';
+  }
+}
+
+/**
+ * @param {integer} number integer number
+ * @return {string} Given a number, return the corresponding day name.
+ */
 function numberToNameDay(number) {
   if(number>6) number = number - 7;
   switch (number) {
@@ -305,5 +373,16 @@ function numberToNameDay(number) {
       break;
     default:
       return 'asdawd';
+  }
+}
+
+var x = window.matchMedia("(min-width: 992px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    var carousel = document.getElementById('carousel');
+    carousel.removeAttribute('data-flickity-options');
   }
 }
